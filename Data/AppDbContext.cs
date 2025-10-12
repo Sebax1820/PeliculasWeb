ff14b340a35d568034507e1978b8505afbabde56
Sebastian <sebax1820@gmail.com>
2025-10-12 13:27:37 -0500

    Creacion de controllers

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculasWeb.Models;
+
+namespace PeliculasWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Director> Directores { get; set; }
+        public DbSet<Actor> Actores { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+    
+}
